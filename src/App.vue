ca67d459261aafe89b65ce14baaaef7a9a6a8861
lateo16 <lateofrederick@gmail.com>
2020-05-18 06:30:44 -0400

    added the router-view for navigation

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,17 @@
 <template>
   <div id="app">
     <NavBar/>
-    <HomePage/>
+    <router-view />
   </div>
 </template>
 
 <script>
-import HomePage from './screens/HomePage'
 import NavBar from './components/NavBar'
 
 
 export default {
   name: 'App',
   components: {
-    HomePage,
     NavBar,
   }
 }
