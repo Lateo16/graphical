96f85b10ccd1abd41d23fa9767b7b4401470ce8f
lateo16 <lateofrederick@gmail.com>
2020-05-18 06:32:40 -0400

    started the plot page

--- src/screens/PlotPage.vue
+++ src/screens/PlotPage.vue
@@ -1,15 +1,29 @@
 <template>
   <div>
       <h1>This is the plot page.</h1>
+      <div class="plot-section">
+      </div>
+      <div class="plot-btns">
+        <PlotBtn btnName="plot"/>
+        <PlotBtn btnName="choose file"/>
+      </div>
   </div>
 </template>
 
 <script>
+import PlotBtn from "../components/PlotBtn";
 export default {
-    name: 'PlotPage'
+    name: 'PlotPage',
+    components: {
+      PlotBtn,
+    }
 }
 </script>
 
 <style scoped>
-
+  .plot-section {
+    width:700px;
+    height: 458px;
+    background-color: green;
+  }
 </style>
